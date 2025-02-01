f14ac5d3f40b4784312ee158292285dd472d3768
kimyujin <jksj0606@naver.com>
2025-02-02 07:47:27 +0900

    [Docs] 햄버거 버튼 클릭 제작

---
+++ js/layout.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// 햄버거 버튼 클릭시
+document.querySelector(".menu").onclick = function () {
+  document.querySelector(".menu").style.display = "none";
+  document.querySelector(".close").style.display = "block";
+  document.querySelector("#gnb02").style.right = "20px";
+};
+
+document.querySelector(".close").onclick = function () {
+  document.querySelector(".menu").style.display = "block";
+  document.querySelector(".close").style.display = "none";
+  document.querySelector("#gnb02").style.right = "-2000px";
+};
