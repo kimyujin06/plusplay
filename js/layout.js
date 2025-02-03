9716c65cc25f66127807a6d0b8489ec523cf8a90
kimyujin <jksj0606@naver.com>
2025-02-03 23:19:51 +0900

    [Fix] 햄버거(메뉴)버튼 클릭 시 메인 스크롤 중복 발생 문제 해결

--- js/layout.js
+++ js/layout.js
@@ -5,10 +5,12 @@ document.querySelector(".menu").onclick = function () {
   document.querySelector(".menu").style.display = "none";
   document.querySelector(".close").style.display = "block";
   document.querySelector("#gnb02").style.right = "20px";
+  document.body.style.overflow = "hidden";
 };
 
 document.querySelector(".close").onclick = function () {
   document.querySelector(".menu").style.display = "block";
   document.querySelector(".close").style.display = "none";
   document.querySelector("#gnb02").style.right = "-2000px";
+  document.body.style.overflow = "auto";
 };
