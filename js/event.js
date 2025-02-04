0629c28799b15a807ec9f5c0c73d6390ae943d62
kimyujin <jksj0606@naver.com>
2025-02-04 23:16:39 +0900

    [Docs] 서브_가이드 모바일 반응형 사이즈 수정 및 모바일 메뉴 기능 추가

--- js/event.js
+++ js/event.js
@@ -1,20 +1,3 @@
-"use strict";
-
-// 햄버거 버튼 클릭시
-document.querySelector(".menu").onclick = function () {
-  document.querySelector(".menu").style.display = "none";
-  document.querySelector(".close").style.display = "block";
-  document.querySelector("#gnb02").style.right = "20px";
-  document.body.style.overflow = "hidden";
-};
-// 햄버거 버튼_x 버튼 클릭시
-document.querySelector(".close").onclick = function () {
-  document.querySelector(".menu").style.display = "block";
-  document.querySelector(".close").style.display = "none";
-  document.querySelector("#gnb02").style.right = "-2000px";
-  document.body.style.overflow = "auto";
-};
-
 //스크롤 이벤트
 AOS.init();
 
---
+++ js/header_menu.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// 햄버거 버튼 클릭시
+document.querySelector(".menu").onclick = function () {
+  document.querySelector(".menu").style.display = "none";
+  document.querySelector(".close").style.display = "block";
+  document.querySelector("#gnb02").style.right = "20px";
+  document.body.style.overflow = "hidden";
+};
+// 햄버거 버튼_x 버튼 클릭시
+document.querySelector(".close").onclick = function () {
+  document.querySelector(".menu").style.display = "block";
+  document.querySelector(".close").style.display = "none";
+  document.querySelector("#gnb02").style.right = "-2000px";
+  document.body.style.overflow = "auto";
+};
