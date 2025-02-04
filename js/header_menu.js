39f5def4d9012e3d8e3f26b5ce56c266f107ddf4
kimyujin <jksj0606@naver.com>
2025-02-04 23:47:44 +0900

    [Test] 서브_가이드 모바일 반응형 사이즈 테스트

--- js/header_menu.js
+++ js/header_menu.js
@@ -4,7 +4,7 @@
 document.querySelector(".menu").onclick = function () {
   document.querySelector(".menu").style.display = "none";
   document.querySelector(".close").style.display = "block";
-  document.querySelector("#gnb02").style.right = "0px";
+  document.querySelector("#gnb02").style.right = "20px";
   document.body.style.overflow = "hidden";
 };
 // 햄버거 버튼_x 버튼 클릭시
