f489040707a555d75629007b2579486af1f05b87
kimyujin <jksj0606@naver.com>
2025-02-06 13:19:25 +0900

    [Fix] push 에러로 인한 재시도

---
+++ js/event.js
@@ -0,0 +1,83 @@
+//스크롤 이벤트
+AOS.init();
+
+// 슬라이드
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+  watchOverflow: true,
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loopAdditionalSlides: 1,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: -195,
+    depth: 250,
+    modifier: 1,
+    slideShadows: true,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    1025: {
+      slidesPerView: 1,
+    },
+    1200: {
+      slidesPerView: 3,
+      coverflowEffect: {
+        rotate: 0,
+        stretch: -145,
+        depth: 250,
+        modifier: 1,
+        slideShadows: true,
+      },
+    },
+    1251: {
+      slidesPerView: 3,
+      coverflowEffect: {
+        rotate: 0,
+        stretch: -155,
+        depth: 250,
+        modifier: 1,
+        slideShadows: true,
+      },
+    },
+    1270: {
+      slidesPerView: 3,
+      coverflowEffect: {
+        rotate: 0,
+        stretch: -165,
+        depth: 250,
+        modifier: 1,
+        slideShadows: true,
+      },
+    },
+    1280: {
+      slidesPerView: 3,
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 300,
+        modifier: 1,
+        slideShadows: true,
+      },
+    },
+  },
+});
