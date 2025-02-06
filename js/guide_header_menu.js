f911c582f10aa541ec8b7a22a3e0b03a65c8f45a
kimyujin <jksj0606@naver.com>
2025-02-07 01:14:12 +0900

    [Docs] 서브_검색 아이콘 클릭 이벤트 반영 완료

--- js/guide_header_menu.js
+++ js/guide_header_menu.js
@@ -14,3 +14,38 @@ document.querySelector(".close").onclick = function () {
   document.querySelector("#gnb02").style.right = "-2000px";
   document.body.style.overflow = "auto";
 };
+
+// 검색 아이콘 클릭시
+document.querySelector(".serch").onclick = function () {
+  document.querySelector(".serch").style.display = "none";
+  document.querySelector(".close02").style.display = "block";
+  document.querySelector("#gnb").style.display = "none";
+  document.querySelector(".serch_screen").style.display = "block";
+  document.querySelector(".serch_wrap").style.transform = "translateY(0px)";
+  document.body.style.overflow = "hidden";
+};
+
+// 검색 아이콘_x 버튼 클릭시
+document.querySelector(".close02").onclick = function () {
+  document.querySelector(".serch").style.display = "block";
+  document.querySelector(".close02").style.display = "none";
+  document.querySelector("#gnb").style.display = "flex";
+  document.querySelector(".serch_screen").style.display = "none";
+  document.querySelector(".serch_wrap").style.transform = "translateY(-5000px)";
+  document.body.style.overflow = "auto";
+};
+
+if (matchMedia("(min-width: 320px) and (max-width: 768px)").matches) {
+  //모바일
+  document.querySelector(".serch_screen").style.display = "none";
+} else if (matchMedia("(min-width: 680px) and (max-width: 1024px)").matches) {
+  //테블릿 sm,md
+  document.querySelector(".serch_screen").style.display = "none";
+} else if (matchMedia("(min-width: 1025px) and (max-width: 1250px)").matches) {
+  //작은 노트북
+  document.querySelector(".serch_screen").style.display = "none";
+}
+//화면크기가 변할때마다 창을 새로고침 시키겠다
+window.onresize = function () {
+  document.location.reload();
+};
